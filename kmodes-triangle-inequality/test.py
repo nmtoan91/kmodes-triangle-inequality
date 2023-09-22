b823c46851e30d01fc2818dc173f9d05743c143c
nmtoan91 <nmtoan91@gmail.com>
2023-09-22 23:18:53 +0900

    save

--- kmodes-triangle-inequality/test.py
+++ kmodes-triangle-inequality/test.py
@@ -10,8 +10,8 @@ from kModesTriangleInequality_IEEEAccess import kModesTriangleInequality_IEEEAcc
 
 dataPath = './DataSample/'
 #dataFile = 'SYN_100000_16_256_8_10.csv'
-#dataFile = 'SYN_512_10_20_8_10.csv'
-dataFile ='SYN_8096_50_50_8_10.csv'
+dataFile = 'SYN_512_10_20_8_10.csv'
+#dataFile ='SYN_8096_50_50_8_10.csv'
 
 
 data = pd.read_csv(dataPath+dataFile, header=None)
